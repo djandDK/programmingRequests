db0fdebdea0b96a287413d910f9a210710b1222e
Ole Friis Nielsen <ole.friis.nielsen@hotmail.com>
2020-12-18 19:27:38 +0100

    changed all characters to lower case.

--- unfinished projects/TruthOrdare/TrueOrDare.ps1
+++ unfinished projects/TruthOrdare/TrueOrDare.ps1
@@ -4,14 +4,14 @@ $questionObject = [pscustomobject]@{}
 if (Test-Path $path -PathType Leaf) {
     $regexMatches = Get-Content -Path $path | select-string -pattern '(?:(?<number>[0-9]+)|(?<letter>[a-zA-Z]+))+.*?:.*?(?<text>\S.*\S)' -AllMatches
     $regexMatches.Matches | Foreach-Object {
-        $questionObject | Add-Member -NotePropertyName $($_.Groups['number'].Value + $_.Groups['letter'].Value) -NotePropertyValue $_.Groups['text'].Value
+        $questionObject | Add-Member -NotePropertyName $($_.Groups['number'].Value + $_.Groups['letter'].Value.ToLower()) -NotePropertyValue $_.Groups['text'].Value
     }
     do {
         $userInput = Read-Host -Prompt 'Enter the number and letter or type exit, to exit'
         Clear-Host
         $regexMatch = $userInput | select-string -pattern '(?:(?<number>[0-9]+)|(?<letter>[a-zA-Z]+))+'
         if ($regexMatch.Matches.Groups[1].Success -eq 'True' -and $regexMatch.Matches.Groups[2].Success -eq 'True') {
-            $numberLetter = $($regexMatch.Matches.Groups[1].Value + $regexMatch.Matches.Groups[2].Value)
+            $numberLetter = $($regexMatch.Matches.Groups[1].Value + $regexMatch.Matches.Groups[2].Value.ToLower())
             try {
                 $question = ($questionObject | Select-Object -ExpandProperty $numberLetter -ErrorAction Stop)
                 write-host "Question with id: $numberLetter"
